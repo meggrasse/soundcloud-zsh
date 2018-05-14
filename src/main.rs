007f16d0be63054a638b27bc17ae19444d06380e
meggrasse <meggrasse@u.northwestern.edu>
2018-05-14 10:11:34 -0500

    Captures URL argument.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+  // do i need this much data?
+  let matches = App::new("soundcloud-zsh")
+    .version("0.1.0")
+    .author("meg grasse <meggrasse@gmail.com>")
+    .about("soundcloud from the command line")
+    .arg(Arg::with_name("URL")
+      .required(true)
+      .takes_value(true)
+      .index(1)
+      .help("url to stream"))
+    .get_matches();
+  // i think unwrapping here is fine since clap should validate arguments
+  let url = matches.value_of("URL").unwrap();
+  println!("{}", url);
 }
