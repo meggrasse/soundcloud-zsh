86f31e63b924a36cdc98ee7d7625b58734a153e7
meggrasse <meggrasse@u.northwestern.edu>
2018-05-14 10:56:21 -0500

    Append env Client ID.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate clap;
 
 use clap::{Arg, App};
+use std::env::{self, VarError};
 
 fn main() {
   // do i need this much data?
@@ -17,4 +18,15 @@ fn main() {
   // i think unwrapping here is fine since clap should validate arguments
   let url = matches.value_of("URL").unwrap();
   println!("{}", url);
+  let client_id_url = append_client_id(url.to_string()).expect("Must have SOUNDCLOUD_CLIENT_ID as an env variable!");
+  println!("{}", client_id_url); 
+}
+
+// todo: make the result have a better error than VarError
+fn append_client_id(mut url: String) -> Result<String, VarError> {
+  // maybe store this env in another way
+  let client_id = env::var("SOUNDCLOUD_CLIENT_ID")?;
+  url.push_str("?client_id=");
+  url.push_str(&client_id);
+  return Ok(url);
 }
